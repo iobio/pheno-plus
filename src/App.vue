d0230e8123de92513222ab87df26145e927e2794
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-09-05 10:08:16 -0600

    Add basic encounter model
    Added the model and logic to render the encounters as a list within the vue app.

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,92 @@
-<script setup>
-  // FHIR.oauth2.ready().then(function(client) {
-  //   // Get encounters for the selected patient
-  //   client.request("/Encounter?patient=" + client.patient.id)
-  //   // Reject if no encounters are found
-  //   .then(function(data) {
-  //       if (!data.entry || !data.entry.length) {
-  //           throw new Error("No encounters found for the selected patient");
-  //       }
-  //       return data.entry;
-  //   })
-  //   // Render the current patient's encounters (or any error)
-  //   .then(
-  //       function(encounters) {
-  //           let numEncounters = encounters.length;
-  //           let encounterContainer = document.getElementById("encounters")
-  //           encounterContainer.innerText = "";
-
-  //           let numEncountersDiv = document.createElement("div");
-  //           numEncountersDiv.innerText = "Number of encounters: " + numEncounters;
-  //           encounterContainer.appendChild(numEncountersDiv);
-  //           //insert a break
-  //           let br = document.createElement("br");
-  //           encounterContainer.appendChild(br);
-
-  //           for (let encounter of encounters) {
-  //               let typeDiv = document.createElement("h3");
-  //               typeDiv.className = "encounter-type";
-  //               let reasonDiv = document.createElement("div");
-  //               let startDiv = document.createElement("div");
-  //               let endDiv = document.createElement("div");
-
-  //               //Make sure the encounter has the required fields if not note that
-  //               if (encounter.resource.type && encounter.resource.type[0] && encounter.resource.type[0].text) {
-  //                   typeDiv.innerText = encounter.resource.type[0].text;
-  //               } else {
-  //                   typeDiv.innerText = "No type found.";
-  //               }
-
-  //               if (encounter.resource.reasonCode && encounter.resource.reasonCode[0].coding[0].display) {
-  //                   reasonDiv.innerText = encounter.resource.reasonCode[0].coding[0].display;
-  //                   reasonDiv.className = "reason";
-  //               } else {
-  //                   reasonDiv.innerText = "No reason found.";
-  //               }
-
-  //               if (encounter.resource.period.start) {
-  //                   startDiv.innerText = encounter.resource.period.start;
-  //               } else {
-  //                   startDiv.innerText = "No start date found.";
-  //               }
-
-  //               if (encounter.resource.period.end) {
-  //                   endDiv.innerText = encounter.resource.period.end;
-  //               } else {
-  //                   endDiv.innerText = "No end date found.";
-  //               }
-
-  //               encounterContainer.appendChild(typeDiv);
-  //               encounterContainer.appendChild(reasonDiv);
-  //               encounterContainer.appendChild(startDiv);
-  //               encounterContainer.appendChild(endDiv);
-  //               let br = document.createElement("br");
-  //               encounterContainer.appendChild(br);
-
-  //               //call the note retrieval function
-  //               getClinicalNote(client, encounter.resource.id);
-  //           };
-  //       },
-  //       function(error) {
-  //           document.getElementById("encounters").innerText = error.stack;
-  //       }
-  //   );
-  // }).catch(console.error);
-
-  // function getClinicalNote(client, encounterId) {
-  //   client.request("/DocumentReference?patient=" + client.patient.id)
-  //   .then(function(data) {
-  //       if (!data.entry || !data.entry.length) {
-  //           console.log(data)
-  //           throw new Error("No clinical notes found for the selected encounter");
-  //       }
-  //       return data.entry;
-  //   })
-  //   .then(function(note) {
-  //       console.log(note);
-  //   })
-  // }
-</script>
-
 <template>
-  <h1>Hello World</h1>
+  <div>{{ "Number of encounters: " + encountersNum }}</div>
+  <br>
+  <div v-for="encounter in encountersList">
+    <h3>{{ encounter.type }}</h3>
+    <p>{{ encounter.reason }}</p>
+    <p>{{ "Start: " + encounter.start + " End: " + encounter.end}}</p>
+    <p>{{ encounter.id }}</p>
+  </div>
+
 </template>
+
+<script>
+  import Encounter from '@/models/Encounter';
+
+  export default {
+    name: 'App',
+    data () {
+      return {
+        encountersList: [],
+        encountersNum: 0,
+      }
+    }, 
+    async mounted () {
+      await this.getInfo();
+    },
+    methods: {
+      async getInfo () {
+        FHIR.oauth2.ready().then((client) => {
+          // Get encounters for the selected patient
+          client.request("/Encounter?patient=" + client.patient.id)
+          // Reject if no encounters are found
+          .then(function(data) {
+              if (!data.entry || !data.entry.length) {
+                  throw new Error("No encounters found for the selected patient");
+              }
+              return data.entry;
+          })
+          // Set the list of encounters & the number of encounters
+          .then(
+              (encounters) => {
+                  console.log(encounters);
+                  for (let encounter of encounters) {
+                      var id = encounter.resource.id;
+                      var encType = null;
+                      var reason = null;
+                      var start = null;
+                      var end = null;
+
+                      //Make sure the encounter has the required fields if not note that
+                      if (encounter.resource.type && encounter.resource.type[0] && encounter.resource.type[0].text) {
+                          encType = encounter.resource.type[0].text;
+                      } else {
+                          encType = "No type found.";
+                      }
+
+                      if (encounter.resource.reasonCode && encounter.resource.reasonCode[0].coding[0].display) {
+                          reason = encounter.resource.reasonCode[0].coding[0].display;
+                      } else {
+                          reason = "No reason found.";
+                      }
+
+                      if (encounter.resource.period.start) {
+                          start = encounter.resource.period.start;
+                      } else {
+                          start = "No start date found.";
+                      }
+
+                      if (encounter.resource.period.end) {
+                          end = encounter.resource.period.end;
+                      } else {
+                          end = "No end date found.";
+                      }
+
+                    //Create the encounter object
+                    var encounterObj = new Encounter(id, encType, reason, start, end);
+                    //Add the encounter to the list of encounters
+                    this.encountersList.push(encounterObj);
+                  };
+              },
+              (error) => {
+                  console.log(error.stack);
+              }
+          );
+        }).catch(console.error);
+      }
+    }
+  }
+</script>
 
 <style scoped>
 
---
+++ src/models/Encounter.js
@@ -0,0 +1,43 @@
+class Encounter {
+    constructor(id, type, reason, start, end) {
+        this.id = id;
+        this.type = type;
+        this.reason = reason;
+        this.start = start;
+        this.end = end;
+    }
+
+    //Getters and Setters
+    getId() {
+        return this.id;
+    }
+    getType() {
+        return this.type;
+    }
+    getReason() {
+        return this.reason;
+    }
+    getStart() {
+        return this.start;
+    }
+    getEnd() {
+        return this.end;
+    }
+    setId(id) {
+        this.id = id;
+    }
+    setType(type) {
+        this.type = type;
+    }
+    setReason(reason) {
+        this.reason = reason;
+    }
+    setStart(start) {
+        this.start = start;
+    }
+    setEnd(end) {
+        this.end = end;
+    }
+}
+
+export default Encounter;
