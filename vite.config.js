780ddbd10b6507e0412fc77f0655e00320d2d41d
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-09-28 13:53:22 -0600

    base not helping at all

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/phenoplus/oauth2/redirect/',
   build: {
     rollupOptions: {
       input: ['index.html', 'startVue.html']
