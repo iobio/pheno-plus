55d2e84d7e3501e363669817c2cfff542c9814dc
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-09-28 12:33:26 -0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import '/src/assets/base.css'
+import './assets/base.css'
 
 import { createApp } from 'vue'
-import App from '/src/App.vue'
+import App from './App.vue'
 
 
 createApp(App).mount('#app')
