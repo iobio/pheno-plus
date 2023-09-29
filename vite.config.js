4aae123a0f799e7a9adeb63c931ff11168238ba3
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-09-29 10:46:55 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,5 +18,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/phenoplus/oauth2/redirect/',
+  // base: '/phenoplus/oauth2/redirect/',
 })
