9a37b45b6e684081fa5ecc79936074045a08dd51
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-09-29 09:53:34 -0600

    change for single page

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   base: '/phenoplus/oauth2/redirect/',
   build: {
     rollupOptions: {
-      input: ['index.html', 'startVue.html']
+      input: ['index.html']
     }
   },
 })
