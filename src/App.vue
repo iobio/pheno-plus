0613756f8a1dac5a112b22a0be14804be27ed8c2
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2023-09-29 10:52:28 -0600

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
       }
     }, 
     async mounted () {
-      await this.getInfo();
+      // await this.getInfo(); //bypass the getting info for now
       let list2 = constructData();
       this.encountersList = this.encountersList.concat(list2);
     },
