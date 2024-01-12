fcc8bf25bd0a1e23b83d7c142b25132afeab906f
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-01-12 10:57:23 -0700

    add path debug

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ const userIdWhitelist = {
     "U0770371": "Phillip Warner"
 };
 
+console.log("pathname: " + window.location.pathname);
 //if the url is the /launch then get the user off the params otherwise it won't be there anyway
 if (window.location.pathname === "/phenoplus/oauth2/launch") {
     //clear out local storage in case there is anything there
