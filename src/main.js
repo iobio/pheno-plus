2f67680fd16e6b0ea6e264f4cd8d2269fe2564a4
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-01-26 13:57:34 -0700

    Add an error message if we cant get the client

--- src/main.js
+++ src/main.js
@@ -58,39 +58,44 @@ if (window.location.hostname === "localhost") {
     app.mount('#app');
 } else {
     //Try to get the FHIR client if we can
-    getClient().then(client => {
-        //If the client is null, we need to try to authorize
-        if (client === null) {
-            FHIR.oauth2.authorize({
-                //Our application's ID
-                client_id: "48f100f1-2599-444b-85f8-5d86b4415453",
-                //Initial scope
-                scope: "launch patient/*.* openid user/*.* profile",
-                //Our redirect URL
-                redirect_uri: redirect_uri,
-                completeInTarget: true,
-            });
-        //If we have a client, we need to check if the user is authorized to use the app
-        } else {
-            let userId = null;
-            try {
-                //Try to get the userId from local storage
-                userId = localStorage.getItem('userId');
-            } catch (error) {
-                //If that fails then do nothing and userId will be null
-            }
-
-            if (!userId || !(userId in userIdWhitelist)) {
-                //If we can't get the userId or it is not in the whitelist, then we need to set the userNotAuthorized flag and mount the app
-                const app = createApp(App)
-                app.config.globalProperties.$userNotAuthorized = true;
-                app.mount('#app');
+    try {
+        getClient().then(client => {
+            //If the client is null, we need to try to authorize
+            if (client === null) {
+                FHIR.oauth2.authorize({
+                    //Our application's ID
+                    client_id: "48f100f1-2599-444b-85f8-5d86b4415453",
+                    //Initial scope
+                    scope: "launch patient/*.* openid user/*.* profile",
+                    //Our redirect URL
+                    redirect_uri: redirect_uri,
+                    completeInTarget: true,
+                });
+            //If we have a client, we need to check if the user is authorized to use the app
             } else {
-                //Call the initializeApp function with the client if it exists & the user is authorized to use the app
-                initializeApp(client);
+                let userId = null;
+                try {
+                    //Try to get the userId from local storage
+                    userId = localStorage.getItem('userId');
+                } catch (error) {
+                    //If that fails then do nothing and userId will be null
+                }
+
+                if (!userId || !(userId in userIdWhitelist)) {
+                    //If we can't get the userId or it is not in the whitelist, then we need to set the userNotAuthorized flag and mount the app
+                    const app = createApp(App)
+                    app.config.globalProperties.$userNotAuthorized = true;
+                    app.mount('#app');
+                } else {
+                    //Call the initializeApp function with the client if it exists & the user is authorized to use the app
+                    initializeApp(client);
+                }
             }
-        }
-    });
+        });        
+    } catch (error) {
+        //Just show a simple error in the console if we cannot get the client that just says "Error getting client"
+        console.error("Error getting client");
+    };
 }
 
 async function initializeApp(fhirClient) {
