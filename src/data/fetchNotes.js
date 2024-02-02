c0df3457a14eb1a7bef86a5802a3de55f9c5d474
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-02-02 12:33:42 -0700

    Update fetchNotes.js

--- src/data/fetchNotes.js
+++ src/data/fetchNotes.js
@@ -43,7 +43,7 @@ export default async function fetchNotes(client, patientId) {
                 //Take the Practitioner/ off the front of the reference
                 authorRef = authorRef.replace('Practitioner/', '');
                 try {
-                    author = await client.request('/PractitionerRole?practitioner=' + authorRef);
+                    author = await client.request('/PractitionerRole/' + authorRef);
                     console.log("Author from request");
                     console.log(author);
                 } catch (error) {
