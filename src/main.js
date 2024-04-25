f7dca3de7f1653bcddd2d6dea82df74738406718
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-04-25 11:23:48 -0600

    Update client_id to match main

--- src/main.js
+++ src/main.js
@@ -77,7 +77,7 @@ import App from './App.vue'
                 try {
                     await FHIR.oauth2.authorize({
                         //Our application's ID
-                        client_id: "48f100f1-2599-444b-85f8-5d86b4415453",
+                        client_id: client_id,
                         //Initial scope
                         scope: "launch patient/*.* openid user/*.* profile",
                         //Our redirect URL
