f7c9a4647cd90a3a281b001b5a180293b1474a3f
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-06-24 12:46:34 -0600

    hard code

--- src/data/fetchNotes.js
+++ src/data/fetchNotes.js
@@ -99,7 +99,7 @@ export default async function fetchNotes(client, patientId) {
             let practitionerId = note.resource && note.resource.author && note.resource.author[0] && note.resource.author[0].reference || null;
             //Clean "Practitioner/" from the string
             // practitionerId = practitionerId && practitionerId.replace("Practitioner/", "");
-            practitionerId = practitionerId;
+            practitionerId = "eAUYipfTmcZzN1s7sUyLfEA3";
             let practitioerRole = 'Not Found';
             let context = 'No context';
             try {
