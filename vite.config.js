c889f0297d5c41d40e4831b0edbdfceff3b21ce2
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2024-06-25 16:15:34 -0600

    Revert
    Reverting to before polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,30 +9,10 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    NodeGlobalsPolyfillPlugin({
-      buffer: true
-    }),
-    NodeModulesPolyfillPlugin()
   ],
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        }),
-        NodeModulesPolyfillPlugin()
-      ]
-    }
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'path': 'path-browserify',
-      'crypto': 'crypto-browserify',
-      'stream': 'stream-browserify',
     }
   },
   base: '/phenoplus/oauth2/redirect/', //staging or dev
