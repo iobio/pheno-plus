b02c0686c0997bc3c0a9dc91d1dafd4ac3eb887b
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-01-29 10:50:15 -0700

    Update fetchNotes.js

--- src/data/fetchNotes.js
+++ src/data/fetchNotes.js
@@ -12,10 +12,8 @@ export default async function fetchNotes(client, patientId) {
     let docSearchUrl =
         '/DocumentReference?patient=' +
         patientId +
-        '&docstatus=preliminary,final,amended&type=http%3A//loinc.org|18842-5,http%3A//loinc.org|11488-4,http%3A//loinc.org|34117-2';
+        '&docstatus=preliminary,final,amended&type=http%3A//loinc.org\\|18842-5,http%3A//loinc.org\\|11488-4,http%3A//loinc.org\\|34117-2';
 
-    // URL encode the search URL
-    docSearchUrl = encodeURI(docSearchUrl);
     console.log(docSearchUrl);
 
     let notes = null;
