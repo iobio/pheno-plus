0bf47b191fb1875ebb9548c78ad1d5ea47abb6e3
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-01-30 13:09:54 -0700

    Update TermPeek.vue

--- src/components/parts/TermPeek.vue
+++ src/components/parts/TermPeek.vue
@@ -68,7 +68,7 @@ export default {
             alertShown: false,
             scrolledIndex: 0,
             lenOfIndexes: 0,
-            isLodingHighlights: true,
+            isLodingHighlights: false,
         };
     },
     methods: {
