f390a0813182262a95bea02ef6194bec52a7119a
Emerson <45885321+emersonlebleu@users.noreply.github.com>
2025-01-30 15:16:44 -0700

    Format config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3002
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/phenoplus/oauth2/redirect/' //staging or dev
-  //   base: '/launch/' //production
-})
+    server: {
+        port: 3002,
+    },
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    base: '/phenoplus/oauth2/redirect/', //staging or dev
+    // base: '/launch/', //production
+});
